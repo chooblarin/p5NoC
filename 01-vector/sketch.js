a3097c8c9716019814ab442d62d811676593ebc7
chooblarin <choo.bla.rin@gmail.com>
2016-12-26 20:55:58 +0900

    Class syntax

--- 01-vector/sketch.js
+++ 01-vector/sketch.js
@@ -1,11 +1,14 @@
-function PVector(x, y) {
-  this.x = x;
-  this.y = y;
-}
+class PVector {
 
-PVector.prototype.add = function(v) {
-  this.x = this.x + v.x;
-  this.y = this.y + v.y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(v) {
+    this.x = this.x + v.x;
+    this.y = this.y + v.y;
+  }
 }
 
 var position = new PVector(100, 100);

9257e53536f3541cead0b86c6655fa536d81332f
chooblarin <choo.bla.rin@gmail.com>
2016-12-26 20:54:46 +0900

    PVector

--- 01-vector/sketch.js
+++ 01-vector/sketch.js
@@ -1,7 +1,15 @@
-var x = 100;
-var y = 100;
-var xSpeed = 1;
-var ySpeed = 3.3;
+function PVector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+PVector.prototype.add = function(v) {
+  this.x = this.x + v.x;
+  this.y = this.y + v.y;
+}
+
+var position = new PVector(100, 100);
+var verocity = new PVector(1, 3.3);
 
 function setup() {
   createCanvas(640, 360);
@@ -10,17 +18,16 @@ function setup() {
 function draw() {
   background(255);
 
-  x = x + xSpeed;
-  y = y + ySpeed;
+  position.add(verocity)
 
-  if ((x > width) || (x < 0)) {
-    xSpeed = xSpeed * -1
+  if ((position.x > width) || (position.x < 0)) {
+    verocity.x = verocity.x * -1
   }
-  if ((y > height) || (y < 0)) {
-    ySpeed = ySpeed * -1
+  if ((position.y > height) || (position.y < 0)) {
+    verocity.y = verocity.y * -1
   }
 
   stroke(0);
   fill(175);
-  ellipse(x, y, 16, 16);
+  ellipse(position.x, position.y, 16, 16);
 }
